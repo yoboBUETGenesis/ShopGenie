db2f9869adce3713bbaf3bd35642b4e7dae7347f
labid-al-nahiyan <labid.nahiyan12@gmail.com>
2024-04-06 11:36:07 +0600

    prduct page design update

--- src/routes/auth/productview/[pid]/+page.svelte
+++ src/routes/auth/productview/[pid]/+page.svelte
@@ -18,6 +18,10 @@
 	function gotocart() {
 		window.open('/auth/cart', '_self');
 	}
+
+	let isOpenReview = false
+	let isOpenSpec = false
+
 </script>
 
 
@@ -126,17 +130,71 @@
 				<p class="text-[70px] leading-0 font-bold leading-tight ">{item.payload.Name}</p>
 				<p ><span class="font-semibold">Category &gt</span> <span class="font-light">{item.payload.Category}</span></p>
 				<p class="text-[24px] text-justify my-4 text-[#5a5a59]">{item.payload.Description}</p>
-				<p class="text-[24px] font-bold">Reviews/Blogs:</p>
-				<ul>
-					<li>Link 1</li>
-					<li>Link2 </li>
-				</ul>
+				
+				<div class="flex justify-between border-b-[2px] border-black p-2">
+					<p class="text-[24px] font-bold  ">
+						Reviews/Blogs
+					</p>
+					<div>
+						{#if isOpenReview}
+							<button on:click={()=>isOpenReview=!isOpenReview}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+								</svg>
+							</button>
+						{:else}
+							<button on:click={()=>isOpenReview=!isOpenReview}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+								</svg>
+						    </button>
+						{/if}
+					</div>
+				</div>
+				{#if isOpenReview}
+					<div class="text-[23px] ml-4 text-justify  text-[#5a5a59]">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam aut excepturi voluptate? Consectetur odio, quas doloremque temporibus reiciendis omnis rerum laborum rem culpa nihil nostrum vel voluptate. Modi alias labore eius aperiam, qui optio architecto eveniet dicta nesciunt dolorum voluptatum!
+					</div>
+				{/if}
+
+
+				<div class="flex justify-between border-b-[2px] border-black p-2">
+					<p class="text-[24px] font-bold  ">
+						Specification
+					</p>
+					<div>
+
+						{#if isOpenSpec}
+							<button on:click={()=>isOpenSpec=!isOpenSpec}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+								</svg>
+							</button>
+						{:else}
+							<button on:click={()=>isOpenSpec=!isOpenSpec}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+								</svg>
+							</button>
+							
+						{/if}
+						  
+					</div>
+					
+				</div>
+				{#if isOpenSpec}
+					<div class="text-[23px] ml-4 text-justify my-4 text-[#5a5a59] mt-0 pt-0 ">
+						{item.payload.Specifications}
+					</div>
+				{/if}
+				
+				
 				<!-- <p class="text-[24px] font-bold">Specification:</p>
 				<p>
 					{item.payload.Specifications}
 				</p> -->
 				
-				<div class="flex flex-col md:flex-row justify-between">
+				<div class="flex flex-col md:flex-row justify-between mt-5">
 					<p class="text-[40px] font-bold">{item.payload.Price}</p>
 					<button class="flex items-center border-[2px] border-black p-3 px-8">
 						<svg
@@ -159,6 +217,13 @@
 			</div>
 			
 		</div>
+	</div>
+
+	<!-- related product -->
+	<div>
+		<p class="text-[40px] mt-5">Related Products  </p>
+
+		<!-- cart show -->
 	</div>
 </div>
 <pre>{JSON.stringify(item, null, 2)}</pre>
